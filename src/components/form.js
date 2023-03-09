a9ade977b3cf1e5e2fc1d6c686f09584e3d75537
muthukumar <muthukumar.s@piquota.com>
2023-03-09 20:41:43 +0530

    Conditional Rendering

--- src/components/form.js
+++ src/components/form.js
@@ -1,45 +1,106 @@
-import React from "react";
-import { useState } from "react";
+import React, {useState} from "react";
+
+// ES6
 
 const Form = () => {
 
-    const [loginForm, setLoginForm] = useState({
-        userName: "",
-        email: "",
-        password: ""
-    });
+    // let [variableName, functionName] = useState(initialValue);
 
-    const handleInput = (event) => {
-        setLoginForm({...loginForm, [event.target.name]: event.target.value})
+    let [userName, setUserName] = useState("");
+    let [loginForm, setLoginForm] = useState({
+        username: '',
+        password: ''
+    });
+    let [inlineCondition, setInlineCondition] = useState(false);
+    let [ifElseCondition, setIfElseCondition] = useState(false);
+    let [passwordVisibility, setPasswordVisibility] = useState(false);
+
+    let message = "Hi there! Good Morning"
+    // let userName;
+
+    const onSubmitForm = () => {
+        console.log(loginForm);
+    };
+    const handleRendering = (condition) => {
+        setInlineCondition(condition)
+        console.log(condition);
     }
-    const onSubmit = (event) => {
-       console.log(loginForm);
+    const handleIfElseRendering = () => {
+         let condition = !ifElseCondition;
+        //  alert(condition);
+        setIfElseCondition(condition);
+    }   
+    
+    const handlePasswordVisibility = (condition) => {
+        setPasswordVisibility(condition);
     }
+    const onHandleInput = (event) => {
+        // setUserName(event.target.value);
+        // console.log(userName);
+        // userName = event.target.value;
+        // console.log(event);
+        // console.log(event.target.value);
+        // console.log(userName);
+        // console.log(event.target.value, event.target.id);
+        setLoginForm({...loginForm, [event.target.id] : event.target.value});
+    };
     return(
+     <div>
+        {/* <h1>This is a simple Form</h1> */}
+        <h1>{message}</h1>
+        <h2>The UserName is {userName}</h2>
+        <h1>Login Info <h2>{loginForm.username}</h2><h2>{loginForm.password}</h2></h1>
+        <div className="space">
+            <label className="label">Enter Your Email ID</label>
+            <input type='email' placeholder='Enter your email' name="email" id="username"  onChange={onHandleInput} />
+        </div>
+        <div className="space">
+            <label className="label">Enter Your Password</label>
+            <input type={passwordVisibility ? "text" : "password"}  placeholder='Enter your Password' onChange={onHandleInput} id="password"/>
+            {
+                passwordVisibility ? <img src={require('../images/open-eye.png')} alt="open-eye" className="password-icon" onClick={() => handlePasswordVisibility(false)} />
+                :
+                <img src={require('../images/close-eye.png')} alt="close-eye" className="password-icon"  onClick={() => handlePasswordVisibility(true)}/>
+            }
+            
+            
+        </div>
+        <div className="space">
+            <button onClick={ () => onSubmitForm()}>Submit Form</button>
+        </div>
         <div>
-            <h2> This is a Simple Form</h2>
-            <h2>The Username is {loginForm.userName}</h2>
-            <h2>The User Email is {loginForm.email}</h2>
-            <h2>The User Password is {loginForm.password}</h2>
-            <div className="space">
-                <label className="label">Enter Your Name</label>
-                <input type="text" placeholder="Enter your name"   onChange={handleInput} name="userName"/>
-                <br></br>
-            </div>
-            <div className="space">
-                <label className="label">Enter Your Email ID</label>
-                <input type="email" placeholder="Enter your mail id" name="email" onChange={handleInput}/>
-                <br></br>
-            </div>
-            <div className="space">
-                <label className="label">Enter Your Password</label>
-                <input type="password" placeholder="Enter your Password" name="password" onChange={handleInput}/>
-                <br></br>
-            </div>
-            <div className="space">
-            <button onClick={() => onSubmit()}>Submit Form</button>
+            <h2>Conditional Rendering</h2>          
+            <button onClick={() => handleRendering(true)}>Show Message</button>
+            <button onClick={() => handleRendering(false)}>Hide Message</button>
+
+            {/* {false && <div>
+                <h1>This is a Inline IF condition</h1>
+                <h1>It will show & hide based on a Condition</h1>
+                </div>
+            }  */}
+            {inlineCondition && <div>
+                <h1>This is a Inline IF condition</h1>
+                <h1>It will show & hide based on a Condition</h1>
+                </div>
+            } 
+            <div>
+                <h1>Inline IF-Else Condition</h1>
+                <button onClick={() => handleIfElseRendering()}>Switch ON/OFF </button>
+                {ifElseCondition ? <img src='https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg?w=2000' alt="" style={{"width" : "100px"}}/> 
+                :
+                <img src='https://thumbs.dreamstime.com/b/light-bulb-11371329.jpg' alt="" style={{"width" : "100px"}}/> }
+                
+                
             </div>
         </div>
+     </div>
     )
 }
 export default Form;
+
+// ES5
+// function SimpleFormSubmition (){
+//     return(
+//         <div></div>
+//     )
+// }
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,46 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
 
 const RegisterPage = () => {
+    let [registerForm, setRegisterForm] = useState({
+        userName: "",
+        emailId: "",
+        password: "",
+        dateOfBirth: "",
+        gender: "",
+        hobbies: [],
+        addresses: "",
+        qualification: "",
+    })
+    const submitRegister = () => {
+        console.log(registerForm);
+    }
+    const handleFormInput = (event) => {
+        if(event.target.name === 'hobbies'){
+            if(event.target.checked){
+                registerForm.hobbies.push(event.target.value)               
+            }
+            else{
+                const index = registerForm.hobbies.indexOf(event.target.value);
+                registerForm.hobbies.splice(index,1);
+            }
+            setRegisterForm({...registerForm, hobbies: registerForm.hobbies})
+        }else{
+            setRegisterForm({...registerForm,[event.target.name] : event.target.value})
+        }
+       
+    }
     return(
         <div>
             <h2>Register Page</h2>
             <div className="space">
                 <label className="register-label">Enter Your Name :</label>
-                <input type="text" placeholder="Enter your name" className="register-input"/>
+                <input type="text" placeholder="Enter your name" className="register-input" onChange={handleFormInput} name="userName"/>
             </div>
             <div className="space">
                 <label className="register-label">Enter Your Email :</label>
-                <input type="email" placeholder="Enter your email id" className="register-input"/>
+                <input type="email" placeholder="Enter your email id" className="register-input" onChange={handleFormInput} name="emailId"/>
             </div>
             <div className="space">
                 <label className="register-label">Enter Your Password :</label>
-                <input type="password" placeholder="Enter your Password" className="register-input"/>
+                <input type="password" placeholder="Enter your Password" className="register-input" onChange={handleFormInput} name="password"/>
             </div>
             <div className="space">
                 <label className="register-label">Select Your DOB :</label>
-                <input type="date" className="register-input"/>
+                <input type="date" className="register-input" onChange={handleFormInput} name="dateOfBirth"/>
             </div>
             <div className="space">
                 <label className="register-label">Select Your Gender :</label>
-                <input type="radio"  className="register-selection" name="gender"/>Male
-                <input type="radio"  className="register-selection" name="gender"/>Female
-                <input type="radio"  className="register-selection" name="gender"/>Others
+                <input type="radio"  className="register-selection" name="gender" onChange={handleFormInput} value="Male" />Male
+                <input type="radio"  className="register-selection" name="gender" onChange={handleFormInput} value="Female"/>Female
+                <input type="radio"  className="register-selection" name="gender" onChange={handleFormInput} value="Others"/>Others
             </div>
             <div className="space">
                 <label className='register-label'>Select Your Hobbies:</label>
-                <input type="checkbox" name="hobbies"/> Cricket
-                <input type="checkbox" name="hobbies"/> Football
-                <input type="checkbox" name="hobbies"/> Badminton
-                <input type="checkbox" name="hobbies"/> Volleyball
-                <input type="checkbox" name="hobbies"/> Boxing
+                <input type="checkbox" name="hobbies" onChange={handleFormInput} value="Cricket"/> Cricket
+                <input type="checkbox" name="hobbies" onChange={handleFormInput} value="Football"/> Football
+                <input type="checkbox" name="hobbies" onChange={handleFormInput} value="Badminton"/> Badminton
+                <input type="checkbox" name="hobbies" onChange={handleFormInput} value="Volleyball"/> Volleyball
+                <input type="checkbox" name="hobbies" onChange={handleFormInput} value="Boxing"/> Boxing
             </div>
             <div className="space">
                 <label className='register-label'>Enter Your Address:</label>
-                <textarea placeholder="Enter Your Address" className="register-input"/>
+                <textarea placeholder="Enter Your Address" className="register-input" onChange={handleFormInput} name="address"/>
             </div>
             <div className="space">
                 <label className='register-label'>Select Your Qualification :</label>
-                <select className="register-input" defaultValue={""}>
+                <select className="register-input" defaultValue={""} onChange={handleFormInput} name="qualification">
                     <option disabled value="">Please Select Your Qualification</option>
                     <option>Under Graduate</option>
                     <option>Post Graduate</option>
@@ -48,7 +76,7 @@ const RegisterPage = () => {
                 </select>
             </div>
             <div className="space">
-                <button>Create Your Account</button>
+                <button onClick={() => submitRegister()}>Create Your Account</button>
             </div>
         </div>
     )
